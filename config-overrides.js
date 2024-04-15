0c8e4e44ff3caff680fb26c27f6bc255203f5ec4
meomeocoj <minh.nt@orai.io>
2024-04-15 15:46:08 +0700

    feat: update and resolve esm

--- config-overrides.js
+++ config-overrides.js
@@ -1,3 +1,4 @@
+const path = require('path');
 module.exports = {
   webpack: function (config, env) {
     config.resolve.fallback = {
@@ -13,8 +14,20 @@ module.exports = {
       crypto: require.resolve("crypto-browserify"),
       stream: require.resolve("stream-browserify"),
       buffer: require.resolve("buffer"),
-      https: require.resolve("https-browserify")
+      https: require.resolve("https-browserify"),
     };
+    config.resolve.alias = {
+      '@injectivelabs/core-proto-ts': path.resolve(__dirname, 'node_modules/@injectivelabs/core-proto-ts/cjs/index.js'),
+      '@injectivelabs/sdk-ts': path.resolve(__dirname, 'node_modules/@injectivelabs/sdk-ts/dist/cjs/index.js'),
+      '@injectivelabs/utils': path.resolve(__dirname, 'node_modules/@injectivelabs/utils/dist/cjs/index.js'),
+      '@injectivelabs/networks': path.resolve(__dirname, 'node_modules/@injectivelabs/networks/dist/cjs/index.js'),
+      '@injectivelabs/exceptions': path.resolve(__dirname, 'node_modules/@injectivelabs/exceptions/dist/cjs/index.js'),
+      '@injectivelabs/mito-proto-ts': path.resolve(__dirname, 'node_modules/@injectivelabs/mito-proto-ts/cjs/index.js'),
+      '@injectivelabs/dmm-proto-ts': path.resolve(__dirname, 'node_modules/@injectivelabs/dmm-proto-ts/cjs/index.js'),
+      '@injectivelabs/indexer-proto-ts': path.resolve(__dirname, 'node_modules/@injectivelabs/indexer-proto-ts/cjs/index.js'),
+      '@injectivelabs/token-metadata': path.resolve(__dirname, 'node_modules/@injectivelabs/token-metadata/dist/cjs/index.js'),
+      '@injectivelabs/ts-types': path.resolve(__dirname, 'node_modules/@injectivelabs/ts-types/dist/cjs/index.js'),
+    }
 
     return config;
   }
